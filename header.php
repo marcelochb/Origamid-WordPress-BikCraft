18fbf4b862c8787d9a06f1d90227c11cb5ff1a94
Marcelo Block <marcelochb@gmail.com>
2020-06-01 07:50:55 -0300

    feat(SplitHomeAndTemplatePages) Split Home and Template pages

--- header.php
+++ header.php
@@ -2,11 +2,11 @@
 <html lang="pt-br">
 	<head>
 		<meta charset="utf-8">
-		<title>Bikcraft - Bicicletas Personalizadas</title>
+		<title><?php bloginfo('name'); ?></title>
 		<meta name="description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte.">
 
 		<meta property="og:type" content="website"/>
-		<meta property="og:title" content="Bikcraft - Bicicletas Personalizadas"/>
+		<meta property="og:title" content="<?php bloginfo('name'); ?>"/>
 		<meta property="og:description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte."/>
 		<meta property="og:url" content="http://bikcraft.com"/>
 		<meta property="og:image" content="http://bikcraft.com/img/og-image.png"/>
@@ -28,10 +28,10 @@
 				</a>
 				<nav class="grid-12 header_menu">
 					<ul>
-						<li><a href="sobre.html">Sobre</a></li>
-						<li><a href="produtos.html">Produtos</a></li>
-						<li><a href="portfolio.html">Portfólio</a></li>
-						<li><a href="contato.html">Contato</a></li>
+						<li><a href="/bikcraft/sobre">Sobre</a></li>
+						<li><a href="/bikcraft/produtos">Produtos</a></li>
+						<li><a href="/bikcraft/portfolio">Portfólio</a></li>
+						<li><a href="/bikcraft/contato">Contato</a></li>
 					</ul>
 				</nav>
 			</div>
